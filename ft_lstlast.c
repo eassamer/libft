0a4a1161cf9b93ac47a419d655006acc943ddc20
Eassamer <68176483+mejdi1@users.noreply.github.com>
2021-11-08 00:42:19 +0100

    ft_lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -12,4 +12,15 @@
 
 #include"libft.h"
 
-t_list
+t_list  *ft_lstlast(t_list *lst)
+{
+        while(lst != NULL)
+        {
+                if(!lst->next)
+                {
+                        return (lst);
+                }
+                lst = lst->next;
+        }
+        return (lst);
+}
